79c35c6eaf57881dd8eaf31dd1be8f90e5bb6e6e
Søren <soeren@langkilde.com>
2023-12-28 11:15:21 +0100

    Test script to test actions

---
+++ source/test.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def area_of_circle(radius: float | None = None) -> float:
+    """Calculate the area of a circle based on radius.
+
+    Args:radius (float | None, optional): Radius of circle. Defaults to None.
+
+    Returns:float: Area of cirlce.
+    """
+    return radius**2 * 3.14

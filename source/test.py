6acc4fd391301d6052dbff12e928c824f1db2dce
Søren <soeren@langkilde.com>
2023-12-28 11:59:51 +0100

    made mistake in documentation on purpose

--- source/test.py
+++ source/test.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def area_of_circle(radius: float | None = None) -> float:
-    """Calculate the area of a circle based on radius.
+    """Calculate the area of a circle based on radius
 
     Args:radius (float | None, optional): Radius of circle. Defaults to None.
 
